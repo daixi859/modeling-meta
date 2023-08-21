2a7a6f439bcf14b2a8e0d01b6c7e20c58c906f48
daixi <daixi859@163.com>
2023-08-21 17:55:13 +0800

    提交

--- views/Index/index.tsx
+++ views/Index/index.tsx
@@ -5,76 +5,24 @@ import useSetState from "@/utils/useSetState";
 import React, { useEffect, useMemo } from "react";
 
 import CellKeyword from "@/views/Index/CellKeyword";
-import { Input, Space, Segmented, Tag, Button } from "antd";
+import { Input, Space, Segmented, Tag, Select, Breadcrumb } from "antd";
 import SimpleTable from "@/components/SimpleTable";
 import classes from "./index.module.scss";
 type Fields = React.ComponentProps<typeof SimpleTable>["columns"];
+const host = process.env.NEXT_PUBLIC_MODELHOST;
 
-const fields: Fields = [
-  {
-    title: "页面/组件/脚本名称",
-    dataIndex: "name",
-    render(name, recode) {
-      return (
-        <>
-          {recode.type === "page" ? (
-            <Tag color="red">页面</Tag>
-          ) : recode.type == "component" ? (
-            <Tag color="blue">组件</Tag>
-          ) : (
-            <Tag color="green">脚本</Tag>
-          )}
-          {name}
-        </>
-      );
-    },
-  },
-  { title: "业务单元", dataIndex: "functionCode" },
-  { title: "id", dataIndex: "id" },
-  { title: "文件名", dataIndex: "filename" },
-  { title: "修改人", dataIndex: "modifiedBy" },
-  {
-    title: "修改时间",
-    dataIndex: "modifiedOn",
-    render(value: string) {
-      return new Date(value).toLocaleString();
-    },
-  },
-  {
-    title: "操作",
-    dataIndex: "action",
-    className: classes.action,
-    width: 150,
-    render(_, row) {
-      const host = process.env.NEXT_PUBLIC_MODELHOST;
-      const typeUrlMap: LooseObject = {
-        page: "function-page-coding",
-        component: "business-component",
-        script: "business-script",
-      };
-      return (
-        <>
-          {row.type === "page" && (
-            <a href={host + row.url} target="_blank">
-              查看
-            </a>
-          )}
-          <a
-            href={host + `/coding/${typeUrlMap[row.type]}/${row.id}`}
-            target="_blank"
-          >
-            编辑
-          </a>
-          <span>引用</span>
-          {row.type !== "page" && <span>被引用</span>}
-        </>
-      );
-    },
-  },
-];
+function hasDependent(row: any) {
+  return (
+    (row.dependentScripts && row.dependentScripts.length > 0) ||
+    (row.dependentComponents && row.dependentComponents.length > 0)
+  );
+}
 
 export default function Page() {
-  const [{ tabs, tab, keyword, list }, setState] = useSetState({
+  const [
+    { tabs, tab, keyword, list, pageId, codeId, functionCode, quoteItems },
+    setState,
+  ] = useSetState({
     list: [] as any[],
     tab: "",
     tabs: [
@@ -84,29 +32,151 @@ export default function Page() {
       { label: "脚本", value: "script" },
     ],
     keyword: "",
+    pageId: "",
+    codeId: "",
+    functionCode: undefined,
+    quoteItems: [] as {
+      type: "q" | "bq";
+      keyword?: string;
+      item: LooseObject;
+    }[],
   });
 
-  const { data } = useMemo(() => {
-    let data = list.filter((t) => (tab ? t.type === tab : true));
+  const fields = useMemo(() => {
+    const fields: Fields = [
+      {
+        title: "页面/组件/脚本名称",
+        dataIndex: "name",
+        render(name, row) {
+          return (
+            <>
+              {row.type === "page" ? (
+                <Tag color="red">页面</Tag>
+              ) : row.type == "component" ? (
+                <Tag color="blue">组件</Tag>
+              ) : (
+                <Tag color="green">脚本</Tag>
+              )}
+              {row.type === "page" ? (
+                <a
+                  href={host + row.url}
+                  target="_blank"
+                  className="hover:text-blue-500"
+                >
+                  {name}
+                </a>
+              ) : (
+                name
+              )}
+            </>
+          );
+        },
+      },
+      { title: "业务单元", dataIndex: "functionCode" },
+      { title: "id", dataIndex: "id" },
+      { title: "文件名", dataIndex: "filename" },
+      { title: "修改人", dataIndex: "modifiedBy" },
+      {
+        title: "修改时间",
+        dataIndex: "modifiedOn",
+        render(value: string) {
+          return new Date(value).toLocaleString();
+        },
+      },
+      {
+        title: "操作",
+        dataIndex: "action",
+        className: classes.action,
+        width: 150,
+        render(_, row) {
+          const typeUrlMap: LooseObject = {
+            page: "function-page-coding",
+            component: "business-component",
+            script: "business-script",
+          };
+          return (
+            <>
+              <a
+                href={host + `/coding/${typeUrlMap[row.type]}/${row.id}`}
+                target="_blank"
+              >
+                编辑
+              </a>
+              {row.dependencies.length > 0 && (
+                <span
+                  onClick={() =>
+                    setState({
+                      quoteItems: quoteItems.concat({ type: "q", item: row }),
+                    })
+                  }
+                >
+                  引用
+                </span>
+              )}
 
-    if (keyword) {
+              {row.bDependencies.length > 0 && (
+                <span
+                  onClick={() =>
+                    setState({
+                      quoteItems: quoteItems.concat({ type: "bq", item: row }),
+                    })
+                  }
+                >
+                  被引用
+                </span>
+              )}
+            </>
+          );
+        },
+      },
+    ];
+    return fields;
+  }, [setState, quoteItems]);
+
+  const { data, quote, kw } = useMemo(() => {
+    let data = list;
+    let kw = keyword;
+    const quote = quoteItems[quoteItems.length - 1];
+    if (quote) {
+      kw = quote.keyword || "";
+      const dependencies =
+        quote.type == "q" ? quote.item.dependencies : quote.item.bDependencies;
+      data = data.filter((t) => dependencies.indexOf(t.id) > -1);
+    } else {
+      data = data
+        .filter((t) => (tab ? t.type === tab : true))
+        .filter((t) =>
+          pageId ? (t.url ? t.url.indexOf(pageId) > -1 : false) : true
+        )
+        .filter((t) => (codeId ? t.id === codeId : true))
+        .filter((t) => (functionCode ? t.functionCode === functionCode : true));
+    }
+
+    if (kw) {
       data = data.filter((item) => {
         let flag = false;
-        if (item.html && item.html.indexOf(keyword) > -1) {
+        if (item.html && item.html.indexOf(kw) > -1) {
           flag = true;
         }
-        if (item.style && item.style.indexOf(keyword) > -1) {
+        if (item.style && item.style.indexOf(kw) > -1) {
           flag = true;
         }
-        if (item.script && item.script.indexOf(keyword) > -1) {
+        if (item.script && item.script.indexOf(kw) > -1) {
           flag = true;
         }
         return flag;
       });
     }
 
-    return { data };
-  }, [list, keyword, tab]);
+    return { data, quote, kw };
+  }, [list, keyword, tab, pageId, codeId, functionCode, quoteItems]);
+
+  const functionCodeOptions = useMemo(() => {
+    return Array.from(new Set(list.map((t) => t.functionCode))).map((t) => ({
+      value: t,
+      label: t,
+    }));
+  }, [list]);
 
   useEffect(() => {
     req
@@ -135,38 +205,131 @@ export default function Page() {
               id: t.scriptGuid,
             }))
           );
+        var bDependencies: Record<string, string[]> = {};
+        list.forEach((t) => {
+          t.dependencies = [];
+          if (t.dependentComponents) {
+            t.dependencies = t.dependencies.concat(
+              t.dependentComponents.map((t: any) => t.componentGuid)
+            );
+          }
+          if (t.dependentScripts) {
+            t.dependencies = t.dependencies.concat(
+              t.dependentScripts.map((t: any) => t.scriptGuid)
+            );
+          }
+          t.dependencies.forEach((id: any) => {
+            console.log("---", id);
+            if (!bDependencies[id]) {
+              bDependencies[id] = [];
+            }
+            if (bDependencies[id].indexOf(t.id) == -1) {
+              bDependencies[id].push(t.id);
+            }
+          });
+        });
+        list.forEach((t) => {
+          t.bDependencies = bDependencies[t.id] || [];
+        });
 
         setState({ list });
       });
   }, [setState]);
-
+  const items = [
+    {
+      title: <span className="cursor-pointer">全部</span>,
+      onClick() {
+        setState({ quoteItems: [] });
+      },
+    },
+  ].concat(
+    quoteItems.map((t, i) => ({
+      title: (
+        <span className="cursor-pointer">
+          {t.item.name + (t.type === "q" ? "-引用" : "-被引用")}
+        </span>
+      ),
+      onClick() {
+        setState({ quoteItems: quoteItems.slice(0, i + 1) });
+      },
+    }))
+  );
   return (
     <PageLayout>
-      <Space className="mb-3">
-        <Segmented
-          options={tabs}
-          value={tab}
-          onChange={(tab) => setState({ tab: tab as string })}
-        />
-        <Input.Search
-          placeholder="请输入代码搜索"
-          allowClear
-          onBlur={(e) => {
-            setState({ keyword: e.target.value });
-          }}
-          onSearch={(keyword) => {
-            setState({ keyword });
-          }}
-        />
-      </Space>
+      {!quote && (
+        <Space className="mb-2 mt-2">
+          <Segmented
+            options={tabs}
+            value={tab}
+            onChange={(tab) => setState({ tab: tab as string })}
+          />
+          <Input.Search
+            placeholder="请输入代码搜索"
+            allowClear
+            onBlur={(e) => {
+              setState({ keyword: e.target.value });
+            }}
+            onSearch={(keyword) => {
+              setState({ keyword });
+            }}
+          />
+          <Select
+            allowClear
+            className="w-36"
+            placeholder="业务单元"
+            value={functionCode}
+            options={functionCodeOptions}
+            onChange={(t) => setState({ functionCode: t })}
+          ></Select>
+          <Input.Search
+            placeholder="编辑ID"
+            allowClear
+            onBlur={(e) => {
+              setState({ codeId: e.target.value });
+            }}
+            onSearch={(codeId) => {
+              setState({ codeId });
+            }}
+          />
+          {(tab === "page" || tab === "") && (
+            <Input.Search
+              placeholder="页面ID"
+              allowClear
+              onBlur={(e) => {
+                setState({ pageId: e.target.value });
+              }}
+              onSearch={(pageId) => {
+                setState({ pageId });
+              }}
+            />
+          )}
+        </Space>
+      )}
+
+      {quote && (
+        <Space className="mb-2 mt-2">
+          <Input.Search
+            placeholder="请输入代码搜索"
+            allowClear
+            onBlur={(e) => {
+              quote.keyword = e.target.value;
+              setState({ quoteItems: quoteItems.slice() });
+            }}
+            onSearch={(keyword) => {
+              quote.keyword = keyword;
+              setState({ quoteItems: quoteItems.slice() });
+            }}
+          />
+          <Breadcrumb items={items} />
+        </Space>
+      )}
+
       <SimpleTable
         columns={fields}
         dataSource={data}
         expandable={{
-          expandedRowRender: (item) => (
-            <CellKeyword keyword={keyword} item={item} />
-          ),
-          rowExpandable: (item, i) => (i === 0 ? !!keyword : false),
+          expandedRowRender: (item) => <CellKeyword keyword={kw} item={item} />,
+          rowExpandable: (item, i) => (i === 0 ? !!kw : false),
         }}
       />
     </PageLayout>
